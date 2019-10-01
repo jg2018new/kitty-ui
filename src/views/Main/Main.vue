3d334eb8d96fe6468bcaf67aa62d8854e228753d
jg2018new <ads-132@163.com>
2019-10-01 12:22:08 +0800

    '头bar'

---
+++ src/views/Main/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    name: "Main",
+    data() {
+      return {}
+    },
+    components: {},
+    methods: {}
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MenuBar/MenuBar.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    name: "MenuBar",
+    data() {
+      return {}
+    },
+    components: {},
+    methods: {}
+  }
+</script>
+
+<style>
+
+</style>
